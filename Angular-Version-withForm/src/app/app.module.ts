a0a10a506bb8e01d3f756dc4b95cf62de21f663f
Xiyao Zhao <xiyao.zhao28@gmail.com>
2021-12-02 20:47:07 -0500

    Angular ReactiveForm Version

---
+++ Angular-Version-withForm/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SelectComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-Version-withForm/src/app/select/select.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormBuilder, NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'app-select',
+    templateUrl: './select.component.html',
+    styleUrls: ['./select.component.css']
+})
+export class SelectComponent implements OnInit {
+    itemList = [
+        'Changjinhu (2021)',
+        'Dune (2021)',
+        'Shang-Chi and the Legend of the Ten Rings (2021)',
+        'Free Guy (2021)',
+        'The Many Saints of Newark (2021)',
+        'Finch (2021)',
+        'Candyman (2021)',
+        'No Time to Die (2021)',
+        'Halloween Kills (2021)',
+    ];
+
+    form: any;
+    renderList = [];
+
+    constructor(private fb: FormBuilder) { }
+
+    get movieList() : FormGroup {
+        return this.form.get("movieList") as FormGroup
+    }
+
+    // Handle check and uncheck Select All
+    selectAll() {
+        this.form.get("selectAll").valueChanges.subscribe((val: boolean) => {
+            if (val === true) {
+                //console.log(Object.values(this.movieList.controls))
+                Object.values(this.movieList.controls).map(val=>val.setValue(true));
+                this.renderList = Object.keys(this.movieList.controls);
+            } else {
+                Object.values(this.movieList.controls).map(val=>val.setValue(false));
+                this.renderList = [];
+            }
+        })
+    }
+
+    // Handle movieList select and display
+    onClick() {
+        this.itemList.map(list => {
+            this.form.get("movieList").get(list)
+                .valueChanges.subscribe(val => {
+                    if (val === true) {
+                        this.renderList.push(list);
+                        // If all movieList are selected, check Select All
+                        if (this.renderList.length === this.itemList.length) {
+                            this.form.controls.selectAll.setValue(true);
+                            // If not all moiveList are selected, uncheck Select All
+                        } 
+                        // else {
+                        //     this.form.controls.selectAll.setValue(false);
+                        // }
+                    } else {
+                        this.renderList.splice(this.renderList.indexOf(list), 1);
+                    }
+                });
+        })
+    }
+
+    // Handle Clear All event
+    clearAll(e: any) {
+       this.form.reset();
+    }
+
+    ngOnInit() {
+        this.form = this.fb.group({
+            selectAll: false,
+            movieList: this.fb.group(
+                this.itemList.reduce((acc, cur) => {
+                    acc[cur] = false;
+                    return acc;
+                }, {})
+            ),
+        });
+        this.movieList;
+        this.selectAll();
+        this.onClick();
+    }
+}
